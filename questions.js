a54d624618b08f2304054ba4375300dccd039dd3
keshav dadhwal <63389604+Keshav1411@users.noreply.github.com>
2021-10-27 15:42:33 +0530

    Add files via upload
    This is my Quiz Application Project With the help of HTML, CSS and Javascript.

---
+++ questions.js
@@ -0,0 +1,71 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The correct sequence of HTML tags for starting a webpage is -",
+    answer: "HTML, Head, Title, Body",
+    options: [
+      "Head, Title, HTML, body",
+      "HTML, Body, Title, Head",
+      "HTML, Head, Title, Body",
+      "HTML, Head, Body, Title"
+    ]
+  },
+    {
+    numb: 3,
+    question: "CSS stands for-",
+    answer: "Cascading style sheets",
+    options: [
+      "Cascade style sheets",
+      "Color and style sheets",
+      "Cascading style sheets",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 4,
+    question: "The property in CSS used to change the background color of an element is -",
+    answer: "background-color",
+    options: [
+      "bgcolor",
+      "color",
+      "background-color",
+      "All of these"
+    ]
+  },
+    {
+    numb: 5,
+    question: "The CSS property used to control the element's font-size is -",
+    answer: "font-size",
+    options: [
+      "text-style",
+      "text-size",
+      "font-size",
+      "None of the above"
+    ]
+  },
+  
+  {
+    numb: 6,
+    question: "Which of the following CSS property is used to set the background image of an element?",
+    answer: "background-image",
+    options: [
+      "background-attachment",
+      "background-image",
+      "background-color",
+      "None of the above"
+    ]
+  },
+  
+];
